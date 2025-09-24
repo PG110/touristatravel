760d686c4ff36c3c9eb212bd97f2559a90cbbfce
Pranav <goelpranav77@gmail.com>
2025-09-24 16:18:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use("/api/package", packageRoute);
 app.use("/api/rating", ratingRoute);
 app.use("/api/booking", bookingRoute);
 
-if (process.env.NODE_ENV_CUSTOM === "production") {
+if (process.env.NODE_ENV_CUSTOM === "https://touristatravel-qujg9xexy-pg110s-projects.vercel.app") {
   //static files
   app.use(express.static(path.join(__dirname, "/client/dist")));
 
